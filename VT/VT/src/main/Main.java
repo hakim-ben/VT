90fa8837a6d4b4c1aa0b2ccbf23f924e8608ef65
benhamed <hakim.benhamed@yahoo.fr>
2025-01-06 14:50:06 +0100

    mettre une seul boucle a la place de 2

--- VT/VT/src/main/Main.java
+++ VT/VT/src/main/Main.java
@@ -3,10 +3,7 @@ package main;
 public class Main {
 
 	public static void main(String[] args) {
-        for(int i = 0; i<5; i++) {
-            System.out.println(i);
-        }
-        for(int i = 5; i<10; i++) {
+        for(int i = 0; i<10; i++) {
             System.out.println(i);
         }
 
